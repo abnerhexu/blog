d32043ddbfd45006b4fe8e6ea5a85b8cf120c83c
abnerhexu <hexu@rochexp.com>
2024-11-06 09:03:27 +0800

    add mobile compatible view

--- blog/pages/about.py
+++ blog/pages/about.py
@@ -33,12 +33,23 @@ def hobby_content() -> rx.Component:
 
 def friends_content() -> rx.Component:
     return rx.vstack(
-        rx.heading("Friends"),
-        rx.grid(
-            rx.foreach(friends, lambda v: rx.card(rx.vstack(rx.text(v[0]), rx.text(v[2]), rx.link("go", href=v[1])))),
-            columns="4"
+        rx.desktop_only(
+            rx.vstack(
+                rx.heading("Friends"),
+                rx.grid(
+                    rx.foreach(friends, lambda v: rx.card(rx.vstack(rx.text(v[0]), rx.text(v[2]), rx.link("go", href=v[1])))),
+                    columns="4"
+                )
+            )
+        ),
+        rx.mobile_and_tablet(
+            rx.heading("Friends"),
+                rx.grid(
+                    rx.foreach(friends, lambda v: rx.card(rx.vstack(rx.text(v[0]), rx.text(v[2]), rx.link("go", href=v[1])), size="2")),
+                    columns="2"
+                )
         )
-    )
+        )
 
 @rx.page(route="/about", title="About me | Abnerhexu's Blog")
 def about_page() -> rx.Component:
@@ -47,14 +58,28 @@ def about_page() -> rx.Component:
             navbar(),
             rx.color_mode.button(position="top-right"),
             rx.button(rx.hstack(rx.icon("arrow-left"), rx.text("Back")), variant="ghost", on_click=rx.redirect("/"), size="4"),
-            rx.hstack(
-                rx.box(rx.vstack(
-                    poem_content(),
-                    hobby_content(),
-                    friends_content(),
-                ), width="80%"),
-                rx.vstack(rx.heading("Table of contents", size="4"), generate_sidebar_category(["Poem", "Hobby", "Friends"]), width="20%")
+            rx.desktop_only(
+                rx.hstack(
+                    rx.box(rx.vstack(
+                        poem_content(),
+                        hobby_content(),
+                        friends_content(),
+                    ), width="80%"),
+                    rx.vstack(rx.heading("Table of contents", size="4"), generate_sidebar_category(["Poem", "Hobby", "Friends"]), width="20%")
+                )
             ),
+            rx.mobile_and_tablet(
+                rx.vstack(
+                    rx.box(rx.vstack(rx.heading("Table of contents", size="4"), generate_sidebar_category(["Poem", "Hobby", "Friends"]), width="100%"),),
+                    rx.box(
+                       rx.vstack(
+                        poem_content(),
+                        hobby_content(),
+                        friends_content(),
+                    ), width="100%"),
+                    spacing="2em"
+                )
+            )
         ),
         spacing="5em"
     )
